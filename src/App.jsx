5cc54f66686a7fc41fcafb90c063a1dd3e29ad58
Gavin Stirling <gavinstir@gmail.com>
2022-06-14 14:10:46 +0100

    Minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.scss";
-import Button from "./components/Button/Button";
 import CardContainer from "./components/CardContainer/CardContainer";
+import RangeInput from "./components/RangeInput/RangeInput";
+import RadioButtons from "./components/RadioButtons/RadioButtons";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [noOfUsers, setNoOfUsers] = useState(7);
+  const [gender, setGender] = useState("All");
 
-  const getUsers = async () => {
-    const url = "https://randomuser.me/api?results=5";
+  const getUsers = async (userNumber, userGender) => {
+    let url = "https://randomuser.me/api";
+    if (userNumber) {
+      url += `?results=${userNumber}&gender=${userGender}`;
+    }
+
     const res = await fetch(url);
     const data = await res.json();
     setUsers(data.results);
   };
 
+  const handleNoOfUsers = (event) => {
+    setNoOfUsers(event.target.value);
+  };
+
+  const handleGender = (event) => {
+    setGender(event.target.value);
+  };
+
+  // getUsers() causes an infinite loop
+
+  // useEffect()
+  // - A function -> The code which you want to run
+  // - An Array -> Dependency Array --> When you want to run the function
+
+  // [] -> Component is loaded --> Mounting
+  // [Dependency] -> Component is reloaded when the dependency is changed
+
+  useEffect(() => {
+    getUsers(noOfUsers, gender);
+  }, [noOfUsers, gender]);
+
   return (
     <div className="app">
       <h1>Random User Generator</h1>
-      <Button onClick={getUsers} label="Get Random Users" />
+      <RangeInput
+        label="No. of Users"
+        id="range_input"
+        onChange={handleNoOfUsers}
+        value={noOfUsers}
+      />
+      <RadioButtons
+        onChange={handleGender}
+        selected={gender}
+        options={["All", "Male", "Female"]}
+      />
       <CardContainer cards={users} />
     </div>
   );
--- src/components/RangeInput/RangeInput.jsx
+++ src/components/RangeInput/RangeInput.jsx
@@ -3,7 +3,7 @@ import "./RangeInput.scss";
 const RangeInput = ({ min = 1, max = 10, label, id, onChange, value }) => {
   return (
     <div className="range-input">
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id}>{label}: {value}</label>
       <input
         id={id}
         type="range"
